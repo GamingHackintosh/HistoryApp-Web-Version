d7da6be11c9f3184c2e8390f5d3221607def115f
GameHack <sawaretev@gmail.com>
2024-03-05 13:55:07 +1000

    update script

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,21 @@
 document.getElementById('search-btn').addEventListener('click', function() {
     const searchValue = document.getElementById('search-input').value.toLowerCase();
-    fetch('data.json')
-        .then(response => response.json())
+    console.log("Поиск по значению:", searchValue); // Для отладки
+    fetch('../data.json') // Проверьте, что путь правильный
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
+            console.log("Полученные данные:", data); // Для отладки
             const filteredData = data.filter(item => item.keywords.toLowerCase().includes(searchValue) || item.date.includes(searchValue));
+            console.log("Отфильтрованные данные:", filteredData); // Для отладки
             displayResults(filteredData);
+        })
+        .catch(error => {
+            console.error('Произошла ошибка при загрузке data.json:', error);
         });
 });
 
