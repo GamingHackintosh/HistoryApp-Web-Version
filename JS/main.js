0b15e7ed8d05edbf99d724f41f766face1704ea3
GameHack <sawaretev@gmail.com>
2024-03-10 21:57:21 +1000

    update style

--- JS/main.js
+++ JS/main.js
@@ -2,10 +2,18 @@
 document.getElementById('search-btn').addEventListener('click', function() {
     const searchValue = document.getElementById('search-input').value.toLowerCase();
     // Проверяем, не пустое ли поле поиска
-    if (!searchValue.trim()) {
-        alert("Поле пусто"); // Можно заменить alert на другой способ уведомления пользователя
-        return; // Прерываем выполнение функции, если поле пусто
+    if (!searchValue) {
+        alert("Поле пусто");
+        return;
     }
+    
+    // Проверяем, что введенное значение не состоит из одной цифры или одиночной буквы
+    if (searchValue.length < 3) {
+        // Если это одиночная цифра или буква, не выполняем поиск
+        console.log("Поиск одиночных символов или пар символов не поддерживается.");
+        return;
+    }
+    
     
     // Проверяем, что введенное значение не является только годом
     const isYearQuery = searchValue.endsWith('гг.') || searchValue.endsWith('г.') || searchValue.endsWith('г') || searchValue.endsWith('.');
