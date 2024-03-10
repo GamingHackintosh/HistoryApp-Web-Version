e08e31b756005f7fd2356910b7e7724e449be12d
GameHack <sawaretev@gmail.com>
2024-03-10 18:49:58 +1000

    update data.json

--- JS/main.js
+++ JS/main.js
@@ -59,11 +59,6 @@ function displayResults(results) {
     }
 }
 
-/* Фильтр ключевых слов */
-const filteredData = data.filter(item =>
-    item.keywords.toLowerCase().includes(searchValue) ||
-    item.date.includes(searchValue)
-);
 
 
 
@@ -76,7 +71,7 @@ function yearToCentury(year) {
 
 // Функция для отображения событий по ключевому слову века
 function displayEventsForKeyword(keyword) {
-    fetch('data.json') // Убедитесь, что путь к data.json корректен
+    fetch('data.json')
     .then(response => response.json())
     .then(data => {
         // Фильтруем события, содержащие ключевое слово в свойстве keywords
@@ -108,8 +103,32 @@ document.addEventListener('DOMContentLoaded', function() {
     centuryLinks.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
-            const centuryText = link.getAttribute('data-century').trim() + "век"; // Получаем значение data-century и добавляем "век"
+            const centuryText = link.getAttribute('data-century').trim() + "век"; 
             displayEventsForKeyword(centuryText);
         });
     });
 });
+
+
+// Скрываем контейнер результатов по умолчанию
+document.getElementById('results').classList.add('hidden');
+
+// Функция для отображения результатов в контейнере результатов
+function displayResults(results) {
+    const resultsContainer = document.getElementById('results');
+    // Если есть результаты, показываем контейнер и заполняем его
+    if (results.length > 0) {
+        resultsContainer.classList.remove('hidden'); // Убираем класс 'hidden', делаем видимым
+        resultsContainer.innerHTML = ''; // Очищаем предыдущие результаты
+        // Создаем HTML для каждого результата и добавляем в контейнер
+        results.forEach(event => {
+            const eventElement = document.createElement('div');
+            eventElement.className = 'event';
+            eventElement.innerHTML = `<h4>${event.date}</h4><p>${event.event}</p>`;
+            resultsContainer.appendChild(eventElement);
+        });
+    } else {
+        resultsContainer.classList.add('hidden'); // Если результатов нет, скрываем контейнер
+        resultsContainer.innerHTML = 'Ничего не найдено. Попробуйте изменить запрос.';
+    }
+}
