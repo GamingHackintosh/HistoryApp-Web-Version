f083c764d9cf5d30d41953ef3484d34a38ceaef3
GamingHackintosh <sawaretev@gmail.con>
2024-06-20 00:47:36 +1000

    add menu-icon

--- assets/JS/sidebar.js
+++ assets/JS/sidebar.js
@@ -1,3 +1,16 @@
+document.getElementById('menu-icon').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '0px') {
+        sidebar.style.left = '-370px';
+        this.style.left = '0';
+    } else {
+        sidebar.style.left = '0';
+        this.style.left = '370px';
+    }
+});
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const sidebar = document.getElementById("sidebar");
     const resultsContainer = document.getElementById("results");
