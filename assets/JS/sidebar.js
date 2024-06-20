d559a9bc83a87f2e49c7c1cf3460f8a1284e9577
GamingHackintosh <sawaretev@gmail.con>
2024-06-20 18:52:28 +1000

    upd. search

--- assets/JS/sidebar.js
+++ assets/JS/sidebar.js
@@ -1,11 +1,11 @@
 document.getElementById('menu-icon').addEventListener('click', function () {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.left === '0px') {
-        sidebar.style.left = '-370px';
+        sidebar.style.left = '-350px';
         this.style.left = '0';
     } else {
         sidebar.style.left = '0';
-        this.style.left = '370px';
+        this.style.left = '350px';
     }
 });
 
