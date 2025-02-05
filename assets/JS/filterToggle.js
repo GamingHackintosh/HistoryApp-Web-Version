684ece47596b2ad0d67a3ada7c67a6029461e6cf
gamehack <gamehack@Mac-mini.local>
2025-02-06 00:14:21 +1000

    switch to react

--- assets/JS/filterToggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const filterWrapper = document.getElementById("century-filter-wrapper");
-    const filter = document.getElementById("century-filter");
-
-    filter.addEventListener("click", function() {
-        filterWrapper.classList.toggle("open");
-    });
-});
